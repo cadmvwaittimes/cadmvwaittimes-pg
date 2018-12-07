f36c869e2d13c6f6d5f4ad532adaf416e3d14a41
jay ess <mentoce@gmail.com>
2018-12-07 13:27:28 -0800

    Add cadmv.helper package

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='0.0.1',
     author='Josh Saunders',
     author_email='saunders.josh.work@gmail.com',
-    packages=['cadmv', 'cadmv.test'],
+    packages=['cadmv', 'cadmv.test', 'cadmv.helper'],
     # scripts=['bin/stowe-towels.py', 'bin/wash-towels.py'],
     # url='http://pypi.python.org/pypi/TowelStuff/',
     # license='LICENSE.txt',
