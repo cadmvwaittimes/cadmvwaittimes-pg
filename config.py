382b472e750c426280d9b7cd62ba32e4795dda70
jay ess <mentoce@gmail.com>
2022-12-12 00:11:31 -0800

    Format using black formatter

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ import os
 from sqlalchemy import create_engine
 
 
-db_filename = 'cadmv.db'
+db_filename = "cadmv.db"
 basedir = os.path.dirname(__file__)
-db_url = 'sqlite:////' + os.path.join(basedir, db_filename)
+db_url = "sqlite:////" + os.path.join(basedir, db_filename)
 
-engine = create_engine(db_url, echo=False) # leave echo=True while developing
+engine = create_engine(db_url, echo=False)  # leave echo=True while developing
--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from distutils.core import setup
 
 
 setup(
-    name='CaDMV',
-    version='0.0.1',
-    author='Josh Saunders',
-    author_email='saunders.josh.work@gmail.com',
-    packages=['cadmv', 'cadmv.test', 'cadmv.helper'],
+    name="CaDMV",
+    version="0.0.1",
+    author="Josh Saunders",
+    author_email="saunders.josh.work@gmail.com",
+    packages=["cadmv", "cadmv.test", "cadmv.helper"],
     # scripts=['bin/stowe-towels.py', 'bin/wash-towels.py'],
     # url='http://pypi.python.org/pypi/TowelStuff/',
     # license='LICENSE.txt',
-    description='Useful CA DMV-related stuff.',
+    description="Useful CA DMV-related stuff.",
     # long_description=open('README.txt').read(),
     # install_requires=[
     #     "Django >= 1.1.1",
