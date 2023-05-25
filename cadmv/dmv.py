d14f6ccb72e51324dcfe51497403e85e3042f342
jay ess <mentoce@gmail.com>
2023-05-25 14:21:31 -0700

    Update function signatures with type hints and update docstrings

--- cadmv/dmv.py
+++ cadmv/dmv.py
@@ -11,29 +11,40 @@ offices_json_url = base_url + '/foims_offices_min.json'
 wait_times_url = base_url + '/output3.txt'
 
 
-def get_offices_json(url=None, timeout=1):
-    """
-    Makes a request to the offices_json_url URL and retrieves the CA DMV
+def get_offices_json(url: str = None, timeout: float = 1.0) -> list[dict]:
+    """Makes a request to the offices_json_url URL and retrieves the CA DMV
     offices. timeout is set to 1 second because it seems like the DMV's site
     is slow to respond.
 
     Returns a list of dicts of offices with a lot of information including,
     but not limited to, name, address, hours, branch number, etc. See
     offices.py for an example of the response is returned.
+
+    Args:
+        url (str, optional): The URL to query. Defaults to None.
+        timeout (float, optional): The length of the timeout. Defaults to 1.0.
+
+    Raises:
+        requests.exceptions.HTTPError: Raised if the response's status code is
+        not 200 (OK)
+
+    Returns:
+        list[dict]: offices with a lot of information including, but not limited
+        to, name, address, hours, branch number, etc. See offices.py for an
+        example of the response is returned
     """
     if url is None:
         url = offices_json_url
 
     resp = requests.get(url, timeout=timeout)
     if not resp.ok:
-        raise requests.exceptions.HTTPError
+        raise requests.exceptions.HTTPError()
 
     return resp.json()['foims_offices']['offices']
 
 
-def get_wait_times(url=None, timeout=1):
-    """
-    Makes a request to the wait_times_url URL and retrieves the wait times
+def get_wait_times(url: str = None, timeout: float = 1.0) -> list[dict]:
+    """Makes a request to the wait_times_url URL and retrieves the wait times
     for each CA DMV office. timeout is to 1 second because it seems like the
     DMV's site is slow. The wait times request comes in as
 
@@ -64,10 +75,16 @@ def get_wait_times(url=None, timeout=1):
         }
     ]
 
-    :param url:         (str) the url to make the request for the wait times
-    :param timeout:     (int) time to wait until the request times out
-    :return wait_times: (list) of dicts of wait times for every branch in the
-                        format given above
+    Args:
+        url (str, optional): The URL to query. Defaults to None.
+        timeout (float, optional): The length of the timeout. Defaults to 1.0.
+
+    Raises:
+        requests.exceptions.HTTPError: Raised if the response's status code is
+        not 200 (OK)
+
+    Returns:
+        list[dict]: wait times for every branch in the format given above
     """
     if url is None:
         url = wait_times_url
@@ -77,7 +94,7 @@ def get_wait_times(url=None, timeout=1):
     if not resp.ok:
         raise requests.exceptions.HTTPError
 
-    text = resp.text.split('\r\n')
+    text = resp.text.split('\r\n')[1:]
     del text[0] # remove the header
 
     wait_times = data.prep_wait_times_data(text, now)
